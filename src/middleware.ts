0cfc7a3fe14daf5cb5ababe042e4c23a8d106dfb
Yuvaraj-0 <yuvaraj525354@gmail.com>
2025-10-08 21:44:59 +0530

    Update everything

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,26 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
+  '/sign-in',
+  '/sign-up',
+  '/sso-callback',
   '/api/webhooks/clerk',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/sso-callback(.*)'
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isPublicRoute(req)) return NextResponse.next();
+
   const { userId } = await auth();
   if (!userId) {
     return NextResponse.redirect(new URL('/sign-in', req.url));
   }
+
   return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/api/(.*)',
   ],
 };
